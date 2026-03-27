ddad173eb21bb9102211984596288752028559a2
sdtppdy <1061925564@qq.com>
2026-03-27 20:29:47 +0800

    高斯消元

--- app/test/complex/main.cpp
+++ app/test/complex/main.cpp
@@ -8,35 +8,30 @@ using namespace std;
 #define EPS 10e-9
 
 vector<complex<double>> solve(vector<vector<complex<double>>>& x, vector<complex<double>>& y) {
+    complex<double> a, b, c;
 
-    for (int i = 0; i < 3; ++i) {
-        complex<double> val = x[i][0];
+    for (int i = 0; i < 2; ++i) {
 
-        for (int j = 0; j < 3; ++j) {
-            x[i][j] /= pow(val, 2 - i); // x ** 2 // x[i][0]
+        complex<double> val = x[i][i];
+        for (int j = i; j < 3; ++j) {
+            x[i][j] /= val;
         }
-
         y[i] /= val;
-    }
 
-    for (int i = 2; i > 0; --i) {
-        y[i] -= y[0];
-        for (int j = 0; j < 3; ++j) {
-            x[i][j] -= x[0][j];
+        for (int j = 2; j > i; --j) {
+            complex<double> factor = x[j][i];
+            y[j] -= (factor * y[i]);
+            for (int k = i; k < 3; ++k) {
+                x[j][k] -= (factor * x[i][k]);
+            }
         }
     }
 
-    for (int i = 1; i < 3; ++i) {
-        complex<double> val = x[i][1];
+    c = y[2] / x[2][2];
+    b = (y[1] - x[1][2] * c) / x[1][1];
+    a = (y[0] - x[0][1] * b - x[0][2] * c) / x[0][0];
 
-        for (int j = 0; j < 3; ++j) {
-            x[i][j] /= pow(val, 2 - i); // x ** 2 // x[i][0]
-        }
-
-        y[i] /= val;
-    }
-
-    // return {a, b, c};
+    return {c, b, a};
 }
 
 signed main() {
@@ -51,13 +46,21 @@ signed main() {
     vector<complex<double>> matrix_y(3);
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            matrix_x[i][j] = pow(xy[(i - 1) * 2], j);
+            matrix_x[i][j] = pow(xy[i * 2], j);
         }
 
-        matrix_y[i] = xy[(i - 1) * 2 + 1];
+        matrix_y[i] = xy[i * 2 + 1];
     }
 
     vector<complex<double>> result = solve(matrix_x, matrix_y);
+
+    for (auto& c : result) {
+        if (fabs(c.real()) < EPS)
+            c.real(0);
+        if (fabs(c.imag()) < EPS)
+            c.imag(0);
+        printf("%.6lf %.6lf\n", c.real(), c.imag());
+    }
 
     return 0;
 }
--- app/test/temp/main.cpp
+++ app/test/temp/main.cpp
@@ -1,52 +1,15 @@
 #include <iostream>
-
-#include <cmath>
-
+#include <unordered_set>
 using namespace std;
 
-class Point { // Point类定义
-  public:     // 外部接口
-    Point(int newx = 0, int newy = 0, int times = 0) {
-        x = newx;
-        y = newy;
-        TimesofGetMysize = times;
-    }
+int main() {
+    unordered_set<int> s;
+    bool ans = 1;
+    s.insert(1);
 
-    int& getX() { return x; }
-    int getX() const { return x; }
-    int& getY() { return y; }
-    int getY() const { return y; }
+    auto it = s.insert(1);
+    ans = ans && it.second;
 
-    int getMysize() {
-        TimesofGetMysize++;
-        return Mysize;
-    }
-
-    int getTimesofGetMysize() { return TimesofGetMysize; }
-
-    friend float dist(const Point& p1, const Point& p2);
-    friend float dist(Point& p1, const Point& p2);
-
-  private: // 私有数据成员，数据成员的定义没有错
-    int x, y;
-    int TimesofGetMysize;
-    const int Mysize = sizeof(Point);
-};
-
-void TestFun(Point p) {
-    cout << "TestFun is " << p.getX();
-}
-
-float dist(const Point& p1, Point& p2) {
-    double x = p1.getX() - p2.getX();
-    double y = p1.getY() - p2.getY();
-    p2.getX()++;
-    return static_cast<float>(sqrt(x * x + y * y));
-}
-
-float dist(Point& p1, const Point& p2) {
-    double x = p1.getX() - p2.getX();
-    double y = p1.getY() - p2.getY();
-    p1.getX()++;
-    return static_cast<float>(sqrt(x * x + y * y));
+    cout << ans << endl;
+    
 }
